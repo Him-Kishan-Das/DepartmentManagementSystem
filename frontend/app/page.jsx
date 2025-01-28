68252da4543ab4dc695fa799d78a75a6a3a769e0
Him Kishan Das <70309670+Him-Kishan-Das@users.noreply.github.com>
2025-01-28 23:00:38 +0530

    First Commit

---
+++ frontend/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div className={styles}>
+      <Navbar />
+
+   </div>
+  );
+}
