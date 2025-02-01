6f884932aebe18ae82828814fa3ead77685e178a
Him Kishan Das <70309670+Him-Kishan-Das@users.noreply.github.com>
2025-02-02 03:24:56 +0530

    Designing of the NotificationPanel

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../components/Navbar";
+import NotificationPanel from "../components/NotificationPanel";
 import "./globals.css";
 
 export default function Home() {
   return (
     <div className={styles}>
       <Navbar />
+      <div className="p-9">
+        <NotificationPanel />
+
+      </div>
 
    </div>
   );
